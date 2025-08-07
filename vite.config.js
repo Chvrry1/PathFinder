2265c51850f35a5b609f93094d356cafa8296688
Chvrry1 <chvrry69@gmail.com>
2025-08-07 11:13:21 -0500

    v1.0.2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'https://Chvrry1.github.io/PathFinder/'
+  plugins: [react()]
 })
